eb8749f9853feddda5e99d6dd19f5522582a657e
AmandeepKainth1234 <122454221+AmandeepKainth1234@users.noreply.github.com>
2023-01-17 14:52:08 +0000

    Add files via upload

--- door_monitor.py
+++ door_monitor.py
@@ -1,59 +1,55 @@
 #! /usr/bin/python3.9
+
+#Import adapted SMS functionality code 
 from SMS import send_sms
+
 import pickle
 from time import sleep
-from ds18b20 import DS18B20
-import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
-GPIO.setmode(GPIO.BCM) 
-GPIO.setup(1, GPIO.OUT, initial=GPIO.LOW)  
-GPIO.setup(9, GPIO.OUT, initial=GPIO.LOW)  
+import RPi.GPIO as GPIO  
 
-#!/usr/bin/env python3
+#Use BCM rather than Board numbering 
+GPIO.setmode(GPIO.BCM) 
+
+GPIO.setup(1, GPIO.OUT, initial=GPIO.LOW)  #Buzzer
+GPIO.setup(9, GPIO.OUT, initial=GPIO.LOW)  #Light
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Reed Switch
+ 
 import signal
 import sys
 import RPi.GPIO as GPIO
-sensors = []
-for sensor_id in DS18B20.get_available_sensors():
-    sensors.append(DS18B20(sensor_id))
-Contact_GPIO = 11
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
+
+
+    
 def contact_callback(channel):
+    #Runs on callback from GPIO event detection 
+    
+    #Read alarm system arming variable 
     arming = pickle.load(open("armingStoringFile.dat", "rb"))
+    
     sleep(0.2)
     if GPIO.input(11):
-    
+        #If reed switch input is high, door was opened
+        
         GPIO.output(9, GPIO.HIGH)  #Turn on the internal light 
         if arming==1:
+            #If system armed (not unlocked)
             print(" Alarm!")
-            
-
-            
-            #GPIO.output(1, GPIO.HIGH)  # Turn on buzzer alarm
-            #send_sms('07459288940',"Alarm Triggered" )
+           
+            GPIO.output(1, GPIO.HIGH)  # Turn on buzzer alarm
+            #Send alarm SMS
+            send_sms('07459288940',"Alarm Triggered" )
             
     else:
+         #If reed switch input is low, door was closed 
          GPIO.output(9, GPIO.LOW)  #Turn off the internal light
-         print("test light off")
+        
 
 
-    
+#Add new GPIO event detection, for rising or falling on reed switch GPIO 
+#Triggers contact_callback function above when event detected
 
-
-GPIO.setup(Contact_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-GPIO.add_event_detect(Contact_GPIO, GPIO.BOTH,
+GPIO.add_event_detect(11, GPIO.BOTH,
         callback=contact_callback, bouncetime=300)
 
-signal.signal(signal.SIGINT, signal_handler)
-signal.pause()
+signal.pause() #Sleep until a signal is detected 
 
-
-
-while True:
-  #for sensor in sensors:
-     # print(sensor.get_temperature)
-      
-    print("Test")
-    sleep(60) 
