95e11df6b6cf314d2864bd5384c353b65082d9ac
AmandeepKainth1234 <122454221+AmandeepKainth1234@users.noreply.github.com>
2023-01-17 14:33:18 +0000

    Add files via upload

--- button_monitor.py
+++ button_monitor.py
@@ -1,13 +1,17 @@
 #! /usr/bin/python3.9
 
-import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO 
+#Use BCM rather than board numbering 
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(26, GPIO.IN,pull_up_down=GPIO.PUD_UP)
-while True:
-  GPIO.setup(26, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
+#Setup pushbutton GPIO input 
+GPIO.setup(26, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+while True:
+  
+  #If button pressed 
   if GPIO.input(26) == 0:
     print("System Online")
-
-    exec(open('Main.py').read())
+    #Execute the main.py script 
+    exec(open('main.py').read())
       
