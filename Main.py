8f8a13c0ef6b394f1b48984242c8d6e2abf98540
AmandeepKainth1234 <122454221+AmandeepKainth1234@users.noreply.github.com>
2023-01-17 00:01:49 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -141,7 +141,28 @@ def send_sms(phone_no, sms_text):
 *    Code version: 2.2.10
 *    Availability: GitHub: https://github.com/adafruit/Adafruit_CircuitPython_Fingerprint
 *    Usage: Lines 145-158, 373-378
-*    License: MIT License. See file MIT_LICENSE_ADAFRUIT within the main repository.
+*    License: MIT License. See below. The license below applies only to the above stated lines.
+        The MIT License (MIT)
+
+        Copyright (c) 2017 ladyada for Adafruit Industries
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
 ***************************************************************************************
 """
  
